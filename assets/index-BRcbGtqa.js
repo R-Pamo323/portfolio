783a5416584042faf38bdb3e588d30fb773a9b5d
Rodrigo Pamo <rodrigopamo323@gmail.com>
2025-09-12 10:16:22 -0500

    Updates

--- assets/index-BRcbGtqa.js
+++ assets/index-BRcbGtqa.js
@@ -1,4 +1,4 @@
-import{r as X,g as ft}from"./index-4oM9O03q.js";var A={exports:{}},L={exports:{}},k={exports:{}};/*!
+import{r as X,g as ft}from"./index-BkobKzzr.js";var A={exports:{}},L={exports:{}},k={exports:{}};/*!
  * getSize v2.0.3
  * measure size of elements
  * MIT license
